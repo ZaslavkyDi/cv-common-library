cfcc23180007da83e653f37e230bc494c4149d87
DmytroZasl <zaslavsky.dima20@gmail.com>
2024-01-16 16:03:33 +0200

    Remove RMQ dependency and client

--- cv_common_library/message_brokers/rabbitmq/congif/__init__.py
+++
@@ -1,8 +0,0 @@
-from functools import lru_cache
-
-from cv_common_library.message_brokers.rabbitmq.congif.settings import RabbitMQSettings
-
-
-@lru_cache
-def get_rabbitmq_settings() -> RabbitMQSettings:
-    return RabbitMQSettings()
