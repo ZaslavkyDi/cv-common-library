9bebde890cd53d16feb8ff3bd53a6f95847ad839
DmytroZasl <zaslavsky.dima20@gmail.com>
2024-01-19 21:21:02 +0200

    Patch version 0.4.9

--- cv_common_library/registry/registry.py
+++ cv_common_library/registry/registry.py
@@ -3,14 +3,14 @@ from typing import Callable
 from cv_common_library.registry.protocol import RegistryProtocol
 
 
-class Registry[_K, _V: RegistryProtocol](dict[_K, _V]):
+class Registry[_KT, _VT: RegistryProtocol](dict[_KT, _VT]):
     """
     Usage:
     service_registry: Registry[str, FakeService] = Registry()
     """
 
-    def register(self, qualifier: _K) -> Callable[[_V], _V]:
-        def decorator(_cls: _V) -> _V:
+    def register(self, qualifier: _KT) -> Callable[[_VT], _VT]:
+        def decorator(_cls: _VT) -> _VT:
             self[qualifier] = _cls.create_instance()
             return _cls
 
